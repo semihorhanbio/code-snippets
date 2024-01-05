de770f77c5f06d8da27df3faf2fccbe12955c40d
Semih Orhan <sorhan3.14@gmail.com>
2024-01-05 23:58:27 +0300

    revalidate paths

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { db } from '@/db';
 
@@ -17,6 +18,7 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
+  revalidatePath('/');
   redirect('/');
 }
 
@@ -59,6 +61,7 @@ export async function createSnippet(
     }
   }
 
+  revalidatePath('/');
   // Redirect the user back to the root route
   redirect('/');
 }
