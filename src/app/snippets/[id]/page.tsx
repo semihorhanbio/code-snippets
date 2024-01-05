7a4160586c58fabf1be2b3ffc3e60b2f898f7135
Semih Orhan <sorhan3.14@gmail.com>
2024-01-05 13:35:08 +0300

    fetch particular records

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,3 +1,20 @@
-export default function SnippetShowPage(props: any) {
-  return <div>Show a snippet</div>;
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: parseInt(props.params.id),
+    },
+  });
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
 }
