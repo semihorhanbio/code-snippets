53fc9dfea351ea4e0852c57b5de237946607ec81
Semih Orhan <sorhan3.14@gmail.com>
2024-01-05 15:51:54 +0300

    call server actions from snippeet edit form

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,15 +3,20 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
+
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
+
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
   const [code, setCode] = useState(snippet.code);
 
   const handleEditorChange = (value: string = "") => {
     setCode(value);
   };
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -23,6 +28,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
